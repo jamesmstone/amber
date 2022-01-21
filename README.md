634f8341aced9b346d159707fd903a5df6020c9b
James Stone <jamesmstone@hotmail.com>
2022-01-21 22:07:12 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Amber
+
+Tracking amber electricity ussage
+
+
+Inspired by this project: [Git scraping: track changes over time by scraping to a Git repository](https://simonwillison.net/2020/Oct/9/git-scraping/)
+
+
+You can view the db from the [`db`](https://github.com/jamesmstone/amber/tree/db) branch
---
+++ run.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+#set -x # debug: print commands before they are executed
+set -o pipefail
+set -o errexit
+
+makeDB() {
+  local db="$1"
+
+  local dockerGitHistory="git-history"
+  local dockerSQLUtil="sqlite-utils"
+
+  rm "$db" || true
+
+  docker build --tag "$dockerGitHistory" --pull --file git-history.Dockerfile .
+  docker build --tag "$dockerSQLUtil" --pull --file sqlite-utils.Dockerfile .
+
+  docker run \
+    -u"$(id -u):$(id -g)" \
+    -v"$(pwd):/wd" \
+    -w /wd \
+    "$dockerGitHistory" file "$db" events.json # --id id
+
+  docker run \
+    -u"$(id -u):$(id -g)" \
+    -v"$(pwd):/wd" \
+    -w /wd \
+    "$dockerSQLUtil" extract "$db" item sourceOrg sourceFeed
+
+}
+
+commitDB() {
+  local dbBranch="db"
+  local db="$1"
+  local tempDB="$(mktemp)"
+  git branch -D "$dbBranch" || true
+  git checkout --orphan "$dbBranch"
+  mv "$db" "$tempDB"
+  rm -rf *
+  mv "$tempDB" "$db"
+  git add "$db"
+  git commit "$db" -m "push db"
+  git push origin "$dbBranch" -f
+}
+
+
+publishDB() {
+  local dockerDatasette="datasette"
+  docker build --tag "$dockerDatasette" --pull --file datasette.Dockerfile .
+    docker run \
+    -v"$(pwd):/wd" \
+    -w /wd \
+    "$dockerDatasette" \
+    publish vercel "$db" --token $VERCEL_TOKEN --project=amber
+}
+
+run() {
+  local db="events.db"
+  makeDB "$db"
+  publishDB "$db"
+  commitDB "$db"
+
+
+}
+
+run "$@"
